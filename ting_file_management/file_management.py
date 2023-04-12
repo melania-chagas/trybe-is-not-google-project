cb4b1335dad9a9f3a2c1c94c917ae2e8b889370a
Melania P Chagas <melaniapchagas@gmail.com>
2023-04-12 19:09:06 -0300

    feat: implementando a função 'txt_importer' e sua função auxiliar 'check_file_format'

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,26 @@
+import os
+import sys
+
+
+def check_file_format(path_file):
+    # https://www.delftstack.com/pt/howto/python/python-get-file-extension/
+
+    # os.path.splitext(file_path) retorna:
+    # ('statics/arquivo_teste', '.txt')
+    extension = os.path.splitext(path_file)
+    return extension[1]
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação."""
+    extension = check_file_format(path_file)
+    if extension != '.txt':
+        return sys.stderr.write('Formato inválido')
+    try:
+        with open(path_file, 'r') as file:
+            txt_file = file.read()
+            return txt_file.split('\n')
+    except IOError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+
+# print(txt_importer('statics/arquivo_teste.txt'))
