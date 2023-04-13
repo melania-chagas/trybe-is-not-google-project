d96d8a88de53e1af4783c90394579f00a6d91fd7
Melania P Chagas <melaniapchagas@gmail.com>
2023-04-13 13:24:20 -0300

    feat: implementando a função 'search_by_word' e refatorando a função 'get_occurrences'

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,34 +2,44 @@ from ting_file_management.queue import Queue
 from ting_file_management.file_process import process
 
 
-def get_occurrences(word, file_lines):
+def get_occurrences(word, file_lines, keyword):
     occurrences = []
+
     for i, line in enumerate(file_lines):
+
         if word.lower() in line.lower():
-            occurrences.append({
-                'linha': i + 1
-            })
+            occurrence = {'linha': i + 1}
+
+            if keyword == 'search_by_word':
+                occurrence = {
+                    'linha': i + 1,
+                    'conteudo': line
+                }
+
+            occurrences.append(occurrence)
+
     return occurrences
 
 
-def exists_word(word, instance):
+def exists_word(word, instance, keyword=None):
     instance_list = list(instance.queue)
     file_lines = instance_list[0]['linhas_do_arquivo']
 
-    if get_occurrences(word, file_lines) == []:
+    if get_occurrences(word, file_lines, keyword) == []:
         return []
 
     return [{
         'palavra': word,
         'arquivo': instance_list[0]['nome_do_arquivo'],
-        'ocorrencias': get_occurrences(word, file_lines)
+        'ocorrencias': get_occurrences(word, file_lines, keyword)
     }]
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    keyword = 'search_by_word'
+    return exists_word(word, instance, keyword)
 
 
 queue = Queue()
 process('statics/arquivo_teste.txt', queue)
-print(exists_word('de', queue))
+print(search_by_word('de', queue))
